48e9a098d1a3fa67117588f637a754fc93325cda
chenxiaoquan233 <chenxiaoquan233@gmail.com>
2020-05-11 10:43:57 +0800

    update

--- server/server.cpp
+++ server/server.cpp
@@ -45,6 +45,9 @@ server::server(char* addr, int portnum)
 	{
 		perror("bind failed!");
 	}
+
+	int timeout = 1200;
+	setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, (char*)&timeout, sizeof(timeout));
 }
 
 void server::running()
@@ -52,30 +55,52 @@ void server::running()
 	setup();
 	char msg[MSGLEN];
 	memset(msg,0,MSGLEN);
+	int nSize = sizeof(sockaddr);
 	while (true) {
 		int res = -1;
-		int nSize = sizeof(sockaddr);
 		while (res == -1)res = recvfrom(sock, msg, MSGLEN, 0, (struct sockaddr*) &last_client_addr, &nSize);
 		if (res!=-1)
 		{
 			printf("get message:%s\n", msg);
 			handle_request(msg);
-
 			memset(msg, 0, MSGLEN);
+		}
+
+#ifdef _WIN32
+		if (_kbhit())//非阻塞获取用户输入
+		{
+			char temp = _getche();
+			if (temp == 'Q' || temp == 'q')
+				break;
+		}
+#endif
+
+		for (int i = 0; i < USERNUM; i++) {
+			if ((clock() - last_conn_time[i])/CLOCKS_PER_SEC>60);
+			{
+				ticket[i] = false;
+				printf("user %d lost connection,ticket take back\n", i);
+				user--;
+			}
 		}
 	}
 }
+
 
 bool server::setup()
 {
 	memset(password,0,PASSWORDLEN+1);
 	if (!ReadLogFile()) {
-		strncpy(password, randomPASSWORD(), 10);
+		strncpy(password, randomPASSWORD(), PASSWORDLEN);
 		for (int i = 0; i < USERNUM; i++)
 			ticket[i] = false;
 		printf("server start\n");
 	}
-	else printf("server restart\n");
+	else {
+		printf("server restart\n");
+		for (int i = 0; i < USERNUM; i++)
+			last_conn_time[i] = clock();
+	}
 	return true;
 }
 
@@ -90,6 +115,29 @@ void server::handle_request(char* msg)
 
 }
 
+bool server::do_goodbye(char* msg)
+{
+	char back_msg[MSGLEN];
+	memset(back_msg, 0, MSGLEN);
+	int free_NO = atoi(msg);
+	ticket[free_NO] = false;
+	user--;
+	sprintf(back_msg, "THNX thanks for using!");
+	sendto(sock, back_msg, MSGLEN, 0, (struct sockaddr*) & last_client_addr, sizeof(last_client_addr));
+	return true;
+}
+
+bool server::do_connect(char* msg)
+{
+	char back_msg[MSGLEN];
+	memset(back_msg, 0, MSGLEN);
+	int conn_NO = atoi(msg);
+	last_conn_time[conn_NO] = clock();
+	sprintf(back_msg, "CONN connecting with server");
+	sendto(sock, back_msg, MSGLEN, 0, (struct sockaddr*) & last_client_addr, sizeof(last_client_addr));
+	return true;
+}
+
 bool server::do_hello(char * msg)
 {
 	char back_msg[MSGLEN];
@@ -99,7 +147,9 @@ bool server::do_hello(char * msg)
 		if (user == 10)
 			sprintf(back_msg, "FAIL no available ticket");
 		else {
-			sprintf(back_msg, "TICK %d", user);
+			int valid_ticket = 0;
+			while (ticket[valid_ticket]) valid_ticket++;
+			sprintf(back_msg, "TICK %d", valid_ticket);
 			last_conn_time[user] = clock();
 			user++;
 		}
