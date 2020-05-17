4f76e145fd5d3dc921549bb591b0ab081f121250
Luckydog691 <61373921+Luckydog691@users.noreply.github.com>
2020-05-17 10:47:34 +0800

    bug fixed

--- client/client.cpp
+++ client/client.cpp
@@ -25,6 +25,7 @@ client::client(char * addr,int portnum,char *Password)
 bool client::running()
 {
 	if (get_ticket()) {
+		int lost_conn_times = 0;
 		printf("APP start running...\n");
 		printf("Input Q or q to quit\n");
 		//int disconn_times = 0;//if connect failed,disconn_times++,if conn_time>=3,APP exit;if connect valid,disconn_times=0
@@ -33,38 +34,37 @@ bool client::running()
 			if ((clock() - last_conn_time) / CLOCKS_PER_SEC > 20) {
 				last_conn_time = clock();
 				if (!confirm_connect())
+				{
 					printf("Connect with server failed.Maybe server crashed.Waiting for server restart...\n");
-				
+					lost_conn_times++;
+				}
+				if (lost_conn_times >= 3)
+					return false;
 			}
-
-
 #ifdef _WIN32
-			if (_kbhit())//��������ȡ�û�����
+			if (_kbhit())//非阻塞获取用户输入
 			{
 				char temp = _getche();
 				if (temp == 'Q' || temp == 'q')
 					break;
 			}
 #endif
-
 		}
-
 		if (free_ticket())
 			return true;
-
 	}
-	return false;
+	return true;
 }
 
 bool client::get_ticket()
 {
 	char msg[MSGLEN];
-	memset(msg, 0, MSGLEN);
+	memset(msg, 0, sizeof(msg));
 	int addr_len=sizeof(sockaddr);
 	while (true) {
 	sprintf(msg, "HELO %s", password);
 	sendto(sock, msg, MSGLEN, 0, (struct sockaddr*)(&serv_addr), sizeof(serv_addr));
-	memset(msg, 0, MSGLEN);
+	memset(msg, 0, sizeof(msg));
 		for (int i = 0; i < 5; i++) {
 	int ret = recvfrom(sock, msg, MSGLEN, 0, (struct sockaddr*) & serv_addr, &addr_len);
 			if ( ret!= -1) {
@@ -77,40 +77,43 @@ bool client::get_ticket()
 					printf("%s\n",msg+5);
 					return false;
 				}
-				else perror("Received message wrong\n");
+				else {perror("Received message wrong\n");
 				return false;
+				}
 			}
 		}
 		printf("Connect failed!trying to connect again...\n");
-			
+
 	}
 }
 
 bool client::free_ticket()
 {
 	char msg[MSGLEN];
-	memset(msg, 0, MSGLEN);
+	memset(msg, 0, sizeof(msg));
 	sprintf(msg, "GBYE %d", ticket_NO);
 	int addr_len=sizeof(sockaddr);
 	sendto(sock, msg, strlen(msg), 0, (struct sockaddr*)(&serv_addr), sizeof(serv_addr));
-	memset(msg, 0, MSGLEN);
-	for (int i = 0; i < 3; i++)
-		if (recvfrom(sock, msg, MSGLEN, 0, (struct sockaddr*) & serv_addr, &addr_len) != -1) {
+	memset(msg, 0, sizeof(msg));
+	for (int i = 0; i < 5; i++)
+	{
+	    if (recvfrom(sock, msg, MSGLEN, 0, (struct sockaddr*) & serv_addr, &addr_len) != -1) {
 			printf("\nReleased ticket OK.APP exit.\n");
 			ticket_NO = -1;
 			return true;
 		}
+	}
 	printf("\nReleased ticket failed,APP exit.\n");
 }
 
 bool client::confirm_connect()
 {
 	char msg[MSGLEN];
-	memset(msg, 0, MSGLEN);
+	memset(msg, 0, sizeof(msg));
 	sprintf(msg, "CONN %d", ticket_NO);
 	int addr_len=sizeof(sockaddr);
 	sendto(sock, msg, MSGLEN, 0, (struct sockaddr*)(&serv_addr), sizeof(serv_addr));
-	memset(msg, 0, MSGLEN);
+	memset(msg, 0, sizeof(msg));
 
 		for (int i = 0; i < 5; i++) {
 			if (recvfrom(sock, msg, MSGLEN, 0, (struct sockaddr*) & serv_addr, &addr_len) != -1) {
@@ -121,12 +124,13 @@ bool client::confirm_connect()
 				else if (strncmp(msg, "FAIL", 4) == 0) {
 					printf("%s\n", msg + 5);
 				}
-				else perror("Received message wrong\n");
+				else {perror("Received message wrong\n");
 				return false;
+				}
 			}
 		}
-		
-	
+
+
 	return false;
-	
+
 }
--- client/main.cpp
+++ client/main.cpp
@@ -7,8 +7,11 @@ int main(int argc, char** argv) {
 	else if (strlen(argv[3]) != PASSWORDLEN)
 		printf("Password length wrong!");
 	else {
-		client Client(argv[1], atoi(argv[2]), argv[3]);
-		Client.running();
+		while (true) {
+			client Client(argv[1], atoi(argv[2]), argv[3]);
+			if (Client.running())
+				break;
+		}
 	}
 }
 
